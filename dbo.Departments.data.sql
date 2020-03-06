85aa1b54877d8bbbecf41389a5a987abfbf96ae2
RinaSatsu <Vanilin6543@gmail.com>
2020-03-07 00:51:10 +0200

    Added project

---
+++ dbo.Departments.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Departments (Code, Name) VALUES ( N'1', N'Department_1');
+INSERT INTO Departments (Code, Name) VALUES ( N'2', N'Department_2');
+INSERT INTO Departments (Code, Name) VALUES ( N'3', N'Department_3');
+INSERT INTO Departments (Code, Name) VALUES ( N'4', N'Department_4');
